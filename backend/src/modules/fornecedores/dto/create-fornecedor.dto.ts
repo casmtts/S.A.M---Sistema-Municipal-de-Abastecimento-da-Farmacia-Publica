ed1e828abaf1df766c5054c936aa79e6c8a09cb0
Calil Mattos <contatocsmpm@gmail.com>
2026-04-22 11:49:18 -0300

    feat: adicionando crud fornecedores

--- backend/src/modules/fornecedores/dto/create-fornecedor.dto.ts
+++ backend/src/modules/fornecedores/dto/create-fornecedor.dto.ts
@@ -5,61 +5,79 @@ import {
   IsBoolean,
   IsNumber,
   Min,
+  MaxLength,
+  IsNotEmpty,
 } from 'class-validator';
 
 export class CreateFornecedorDto {
   @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
   nome: string;
 
   @IsOptional()
   @IsString()
+  @MaxLength(100)
   razaoSocial?: string;
 
   @IsString()
+  @IsNotEmpty()
+  @MaxLength(18)
   cnpj: string;
 
   @IsOptional()
   @IsString()
+  @MaxLength(20)
   inscricaoEstadual?: string;
 
   @IsOptional()
   @IsString()
+  @MaxLength(200)
   endereco?: string;
 
   @IsOptional()
   @IsString()
+  @MaxLength(10)
   numero?: string;
 
   @IsOptional()
   @IsString()
+  @MaxLength(50)
   complemento?: string;
 
   @IsOptional()
   @IsString()
+  @MaxLength(50)
   bairro?: string;
 
   @IsOptional()
   @IsString()
+  @MaxLength(50)
   cidade?: string;
 
   @IsOptional()
   @IsString()
+  @MaxLength(2)
   uf?: string;
 
   @IsOptional()
   @IsString()
+  @MaxLength(9)
   cep?: string;
 
   @IsOptional()
   @IsString()
+  @MaxLength(20)
   telefone?: string;
 
   @IsOptional()
   @IsEmail()
+  @MaxLength(100)
   email?: string;
 
   @IsOptional()
   @IsString()
+  @MaxLength(100)
   contato?: string;
 
   @IsOptional()
@@ -69,6 +87,7 @@ export class CreateFornecedorDto {
 
   @IsOptional()
   @IsString()
+  @MaxLength(100)
   condicoesPagamento?: string;
 
   @IsOptional()
